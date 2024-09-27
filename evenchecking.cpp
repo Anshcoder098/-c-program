518df41fb2446abacfd2b29de6207fa3730fc31c
Anshcoder098 <ansh88179@gmail.com>
2024-09-27 16:29:19 +0530

    Add files via upload

---
+++ evenchecking.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the value of a  ");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+	
+	printf("even no.");
+}
+	else{
+	
+	printf("odd no.");
+}
+    
+    return 0;
+}
